b3e86d4b9656c23cbc8185404ca0b904cb00fb31
fernandes99 <fernandes.r1999@gmail.com>
2024-02-11 10:59:46 -0300

    feat: add project

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -4,6 +4,7 @@ import CoupverseImage from '@/assets/img/coupverse_20231201.png';
 import PokegoalImage from '@/assets/img/pokegoal_20231201.png';
 import ClickvetImage from '@/assets/img/clickvet_20231201.png';
 import BushFarmImage from '@/assets/img/bushfarm_20231201.png';
+import FernandesDevImage from '@/assets/img/fernandes_dev_20240111.png';
 
 import {
     BiLogoTypescript,
@@ -87,7 +88,7 @@ export const projectList = [
         title: 'Pokeclash',
         description:
             'Um jogo pokemon. O principal propósito desse projeto, era apriomorar minhas habilidades de tipagem usando typescript. Usando a API do PokeApi, foi uma ótima oportunidade para tipar os...',
-        tools: [tool.react, tool.typescript],
+        tools: [tool.react, tool.typescript, tool.styled_component],
         path_image: PokeClashImage,
         github_link: 'https://github.com/fernandes99/pokeclash',
         project_link: 'https://pokeclash.netlify.app/'
@@ -104,16 +105,26 @@ export const projectList = [
     },
     {
         id: '3',
+        title: 'Blog & Portfólio',
+        description:
+            'O projeto que você está visualizando neste site funciona não apenas como um portfólio, mas também como um blog. Foi construído utilizando NextJS e está integrado com DatoCMS.',
+        tools: [tool.nextjs, tool.react, tool.tailwind, tool.typescript, tool.figma],
+        path_image: FernandesDevImage,
+        github_link: 'https://github.com/fernandes99/bushfarm',
+        project_link: 'https://bushfarm.netlify.com/'
+    },
+    {
+        id: '4',
         title: 'Coupverse',
         description:
             'Um projeto front e backend em mono repo, utilizando websocket. O jogo simula um famoso jogo de cartas chamado Coup. Feito completo do protótipo ao código.',
-        tools: [tool.figma, tool.node_js, tool.typescript, tool.react],
+        tools: [tool.figma, tool.node_js, tool.typescript, tool.react, tool.styled_component],
         path_image: CoupverseImage,
         github_link: 'https://github.com/fernandes99/coupverse',
         project_link: 'https://coupverse.vercel.app/'
     },
     {
-        id: '4',
+        id: '5',
         title: 'Pokegoal',
         description:
             'Um jogo clássico de pokemon, onde há sistemas de captura e pokedex desses pequenos bichinhos. Feito completo do protótipo ao código.',
@@ -123,7 +134,7 @@ export const projectList = [
         project_link: 'https://pokegoal.netlify.com/'
     },
     {
-        id: '5',
+        id: '6',
         title: 'Clickvet',
         description:
             'Um sistema simples, onde busca, de acordo com o seu CEP, veterinários próximos com base em dados de uma planilha, utilizando o webhook do google sheets.',
@@ -133,19 +144,10 @@ export const projectList = [
         project_link: 'https://clickvet.netlify.com/'
     },
     {
-        id: '6',
+        id: '7',
         title: 'Bushfarm',
         description:
             'Um jogo de fazenda simples, onde você pode comprar e cultivar plantações. Imitando a ideia de uma mineração de cripto.',
-        tools: [tool.react, tool.typescript, tool.figma],
-        path_image: BushFarmImage,
-        github_link: 'https://github.com/fernandes99/bushfarm',
-        project_link: 'https://bushfarm.netlify.com/'
-    },
-    {
-        id: '7',
-        title: 'Fernandes Dev',
-        description: 'Meu próprio site.',
         tools: [tool.react, tool.typescript, tool.figma],
         path_image: BushFarmImage,
         github_link: 'https://github.com/fernandes99/bushfarm',
